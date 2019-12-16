0e5798fc1b9e0949636fface99d28eae1d40f5d6
seonjuyang <58598677+seonjuyang@users.noreply.github.com>
2019-12-16 22:34:15 +0900

    firebase_android

--- firebase_login/lib/main.dart
+++ firebase_login/lib/main.dart
@@ -6,9 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(color: Colors.blue,),
-      )
-    );
-
+        home: Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    ));
+  }
 }
